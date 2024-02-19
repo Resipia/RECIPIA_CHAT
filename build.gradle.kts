519e042fdcf0fda05d51e3630afe60b0f2053cf0
wlsdks <wlsdks12@naver.com>
2024-02-19 14:06:15 +0900

    [RCP-404] :sparkles: webSocket 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,18 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	// webflux, mongo
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+
+	// webSocket
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
