9886f962f197c09a4c254dfe79a161a990bc8182
wlsdks <wlsdks12@naver.com>
2024-02-23 23:55:45 +0900

    [RCP-404] :sparkles: 리포지토리 코드 추가

--- src/main/kotlin/com/recipia/chat/repository/MessageRepository.kt
+++ src/main/kotlin/com/recipia/chat/repository/MessageRepository.kt
@@ -2,6 +2,8 @@ package com.recipia.chat.repository
 
 import com.recipia.chat.domain.Message
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
 
 interface MessageRepository: ReactiveMongoRepository<Message, String> {
+    fun findByChatRoomId(chatRoomId: String): Flux<Message>
 }
