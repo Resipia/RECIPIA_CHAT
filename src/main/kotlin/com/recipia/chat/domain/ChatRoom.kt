0ba6dd63017c71601cbff12aded02e41ad217758
wlsdks <wlsdks12@naver.com>
2024-02-25 22:38:06 +0900

    [RCP-407] :recycle: 채팅방 식별자 필드 추가 및 서비스 로직 수정 (실행오류 해결)

--- src/main/kotlin/com/recipia/chat/domain/ChatRoom.kt
+++ src/main/kotlin/com/recipia/chat/domain/ChatRoom.kt
@@ -9,6 +9,7 @@ data class ChatRoom(
 
     @Id
     val id: String? = null,  // 채팅방 고유 ID. MongoDB가 자동으로 생성해주므로 nullable로 선언.
+    val roomIdentifier: String, // 채팅방을 식별하는 데 사용되는 필드 (1:1 채팅방을 식별자)
     val memberIds: MutableSet<String> = HashSet(), // 멤버 서버의 사용자 ID 목록. MutableSet을 사용하여 수정 가능하도록 함.
     val createdAt: LocalDateTime? = null,
     val updatedAt: LocalDateTime? = null
