159c5e16e8e4715b7db1df08f1efb93ac0777caf
wlsdks <wlsdks12@naver.com>
2024-03-14 14:20:42 +0900

    [RCP-409] :sparkles: 채팅방 목록 조회 기능 추가

--- src/main/kotlin/com/recipia/chat/controller/ChatMessageController.kt
+++ src/main/kotlin/com/recipia/chat/controller/ChatMessageController.kt
@@ -15,6 +15,8 @@ class ChatMessageController(
     /**
      * 클라이언트가 /app/chat.send를 통해 메시지를 보내면, ChatMessageController의 sendMessage 메소드가 해당 메시지를 처리한다.
      * 이 메소드는 메시지를 MongoDB에 저장하고, 저장된 메시지를 /topic/public으로 전송하여 구독자에게 메시지를 브로드캐스트한다.
+     * @SendTo("/topic/public")는 처리된 메시지를 어디로 전송할지 결정한다.
+     * 메시지가 /topic/public으로 전송되어, 이 주제를 구독하는 모든 클라이언트에게 브로드캐스트되는것이다.
      */
     @MessageMapping("/chat.send")
     @SendTo("/topic/public")
--- src/main/kotlin/com/recipia/chat/repository/ChatRoomRepository.kt
+++ src/main/kotlin/com/recipia/chat/repository/ChatRoomRepository.kt
@@ -2,8 +2,11 @@ package com.recipia.chat.repository
 
 import com.recipia.chat.domain.ChatRoom
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface ChatRoomRepository : ReactiveMongoRepository<ChatRoom, String> {
     fun findByRoomIdentifier(roomIdentifier: String): Mono<ChatRoom>
+
+    fun findAllByMemberIdsContaining(memberId: String): Flux<ChatRoom>
 }
