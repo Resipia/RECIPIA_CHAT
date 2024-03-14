b5cb9fcec464c695f0addf639137beb8d58254c0
wlsdks <wlsdks12@naver.com>
2024-03-14 12:53:06 +0900

    [RCP-409] :sparkles: 코틀린 로깅 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 
+	// log
+	implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+
 	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
